9d8c9432e3b4c266ec04306c0b71ca73300d2a29
jackson-perry <jackson.perry@outlook.com>
2021-09-18 17:10:35 -0400

    Delete python directory

--- python/openwebpage.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 27 16:39:55 2019
-
-@author: jacks
-"""
-
-import webbrowser
-webbrowser.open('https://www.jllis.mil/index.cfm?disp=../admin/import/Import.cfm', new = 2)
